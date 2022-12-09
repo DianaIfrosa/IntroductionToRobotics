299f186305fcfed05aba3e831188b3efec36e700
Ifrosă Diana-Maria <40547787+DianaIfrosa@users.noreply.github.com>
2022-12-09 22:41:22 +0200

    Memory improvements & highscore display
    Deleted some variables to maximize memory and implemented highscore display on LCD

--- Homework/H5/Game/LCD.h
+++ Homework/H5/Game/LCD.h
@@ -16,14 +16,11 @@ const byte displayLength = 16;
 const byte LCDBrightValues[] = { 10, 90, 170, 255 };
 byte LCDBrightness = 0;
 
-const String informativeMessage = "Back:scroll left";
 const String options[] = { "Start game", "Highscore", "Settings", "About", "How to play" };
-const String instructions[] = { "Low difficulty:", "basic", "Medium diff:", "fixed obstacles", "High diff:", "obstacles and", "button press for", "passing walls.", informativeMessage, "" };
-const String staticInfo = "Snake game";
+const String instructions[] = { "Low difficulty:", "basic", "Medium diff:", "fixed obstacles", "High diff:", "obstacles and", "button press for", "passing walls.", "Back:scroll left", "" };
 const String scrollingInfo = "                by Ifrosa Diana-Maria, github.com/DianaIfrosa               ";
 const String settingsOptions[] = { "Difficulty", "LCD brightness", "MTR brightness", "Sound" };
 const String gameOverMessages[] = { "Nice game!", "Don't give up!", "Keep it up!", "You got this!", "Great work!" };
-const String highScoreCongrats[] = { "Congrats! You", "beat highscore!" };
 char nameIntroduced[] = "AAAAA";
 
 byte rightArrow[] = {
@@ -138,27 +135,47 @@ void displayHighscoreList(byte position) {
   lcd.setCursor(0, 0);
   lcd.write(byte(0));  // right arrow
 
-  lcd.setCursor(1, 0);
-  lcd.print(position);
-  lcd.print(". ");
-  lcd.print(getHighscoreName(position));
-  lcd.print(" ");
-  lcd.print(getHighscoreValue(position));
+  // display arrows
+  if (position == 1) {
+    lcd.setCursor(15, 0);
+    lcd.write(byte(2));  // down arrow
+  }
+  if (position > 1) {
+    lcd.setCursor(15, 0);
+    lcd.write(byte(3));  // up arrow
 
-  if (position < noHighscores) {
+    if (position <= noHighscores) {
+      lcd.setCursor(15, 1);
+      lcd.write(byte(2));  // down arrow
+    }
+  }
+
+  if (position <= noHighscores) {
+    // display first line of LCD
+    lcd.setCursor(1, 0);
+    lcd.print(position);
+    lcd.print(". ");
+    lcd.print(getHighscoreName(position));
+    lcd.print(" ");
+    lcd.print(getHighscoreValue(position));
+  }
+
+  if (position <= noHighscores - 1) {
+    // display second line of LCD
     lcd.setCursor(1, 1);
     lcd.print(position + 1);
     lcd.print(". ");
     lcd.print(getHighscoreName(position + 1));
     lcd.print(" ");
     lcd.print(getHighscoreValue(position + 1));
-    lcd.setCursor(15, 1);
-    lcd.write(byte(2));  // down arrow
-  }
-
-  if (position > 1) {
-    lcd.setCursor(15, 0);
-    lcd.write(byte(3));  // up arrow
+  } else if (position == noHighscores) {
+    lcd.setCursor(1, 1);
+    lcd.print("Reset list");
+  } else if (position == noHighscores + 1) {
+    lcd.setCursor(1, 0);
+    lcd.print("Reset list");
+    lcd.setCursor(1, 1);
+    lcd.print("(push button)");
   }
 }
 
@@ -184,7 +201,7 @@ void displaySettingsMenu(byte currentOption) {
 
   if (currentOption == ARRAY_SIZE(settingsOptions) - 1) {
     lcd.setCursor(0, 1);
-    lcd.print(informativeMessage);
+    lcd.print("Back:scroll left");
   }
 }
 
@@ -246,7 +263,7 @@ void displayStaticInfo() {
   lcd.write(byte(1));  // left arrow
 
   lcd.setCursor(2, 0);
-  lcd.print(staticInfo);
+  lcd.print("Snake game");
 }
 
 void displayGame(byte level, byte score, byte lives) {
@@ -305,10 +322,10 @@ void displayGameAchievements(byte score, byte level) {
 
 void displayHighscoreCongrats() {
   lcd.setCursor(1, 0);
-  lcd.print(highScoreCongrats[0]);
+  lcd.print("Congrats! You");
 
   lcd.setCursor(1, 1);
-  lcd.print(highScoreCongrats[1]);
+  lcd.print("beat highscore!");
 }
 
 void displayEnterName(byte positionBar) {
@@ -317,12 +334,12 @@ void displayEnterName(byte positionBar) {
   lcd.print("Name: ");
   lcd.print(nameIntroduced);
 
+  lcd.setCursor(15, 0);
+  lcd.write(byte(5));  // up-down arrow
+
   // mark the current letter selected
   lcd.setCursor(6 + positionBar, 1);
   lcd.write(byte(6));  // bar
-
-  // lcd.setCursor(0, 1);
-  // lcd.print("Press: save&exit");
 }
 
 
